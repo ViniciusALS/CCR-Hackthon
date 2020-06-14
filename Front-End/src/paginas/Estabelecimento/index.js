3b42d0138619dff80f8bf5a48ce6f3c17ad15cd5
Vinicius <me@viniciusals.com>
2020-06-14 11:33:03 -0300

    Cria logica para receber valores da api

--- Front-End/src/paginas/Estabelecimento/index.js
+++ Front-End/src/paginas/Estabelecimento/index.js
@@ -16,24 +16,54 @@ const Estabelecimento = (props) => {
 
     const estabelecimentoId = props.match.params.id;
 
-    const [titulo, setTitulo] = useState("Posto do Seu Zé");
-    const [descricao, setDescricao] = useState("O melhor posto da região!");
-    const [nota, setNota] = useState(3);
-    const [endereco, setEndereco] = useState("Rua Melo Augusta");
-    const [latitude, setLatitudo] = useState(-22.9108709);
-    const [longitude, setLongitude] = useState(-43.2190417);
-    const [disponibilidade, setDisponibilidade] = useState();
-    const [reviews, setReviews] = useState();
+    const [titulo, setTitulo] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [notaGeral, setNotaGeral] = useState(0);
+    const [endereco, setEndereco] = useState("");
+    const [latitude, setLatitude] = useState(0);
+    const [longitude, setLongitude] = useState(0);
+    const [disponibilidade, setDisponibilidade] = useState({});
+    const [reviews, setReviews] = useState({});
 
     useEffect(() => {
         api.get(`estabelecimentos/${estabelecimentoId}`)
             .then(response => {
-                console.log(response.data);
-            });
-        }, []);
+                setTitulo(response.data.nome);
+                setDescricao(response.data.descricao);
+                setNotaGeral(response.data.notaGeral);
+                setEndereco(response.data.endereco);
+                setLatitude(response.data.latitude);
+                setLongitude(response.data.longitude);
+                setDisponibilidade(response.data.disponibilidades);
+                setReviews(response.data.reviews);
+            
+            }).catch(function (error) {
+                console.log(error);
 
-    const posicao = [latitude, longitude];
-    
+                setTitulo("Posto do Seu Zé");
+                setDescricao("O melhor posto da região!");
+                setNotaGeral(3);
+                setEndereco("Rua Melo Augusta");
+                setLatitude(-22.9108709);
+                setLongitude(-43.2190417);
+                setDisponibilidade(
+                    [
+                        { "item": "Gasolina", "imagem": "file:///C:/Users/vinic/Desktop/gas-station.svg"},
+                        { "item": "Banheiro", "imagem": "file:///C:/Users/vinic/Desktop/toilet.svg"},
+                        { "item": "Chuveiro", "imagem": "file:///C:/Users/vinic/Desktop/shower.svg"},
+                        { "item": "Cafeteria", "imagem": "file:///C:/Users/vinic/Desktop/cafe.svg"}
+                    ]);
+                setReviews(
+                    [
+                        { "usuario": "Carlos", "nota": 3, "comentario": "Bem okay." },
+                        { "usuario": "Maria", "nota": 5, "comentario": "Recomendo, muito bom atendimento." },
+                        { "usuario": "Felipe", "nota": 1, "comentario": "O café estava frio." }
+                    ]
+                );
+            }); 
+        }, []
+    );
+
     const greenIcon = L.icon({
         iconUrl: MarkerIcon,
         iconSize:     [55, 55]
@@ -44,17 +74,17 @@ const Estabelecimento = (props) => {
             <Header/>
             <main>
 
-                <Map className="map" center={posicao} zoom={17} animate={false} zoomControl={false}>
+                <Map className="map" center={[latitude, longitude]} zoom={17} animate={false} zoomControl={false}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                    <Marker position={posicao} icon={greenIcon}/>
+                    <Marker position={[latitude, longitude]} icon={greenIcon}/>
                 </Map>
 
                 <h1>{titulo}</h1>
                 <p>{descricao}</p>
 
-                <Nota/>
+                <Nota pontuacao={notaGeral} />
 
                 <p>{endereco}</p>
                 
