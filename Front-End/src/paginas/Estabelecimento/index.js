24cee546bb3ebc72a591ca823477cee199782d26
Vinicius <me@viniciusals.com>
2020-06-14 01:50:23 -0300

    Atualiza pagina do estabelecimento

--- Front-End/src/paginas/Estabelecimento/index.js
+++ Front-End/src/paginas/Estabelecimento/index.js
@@ -1,42 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import { Link } from 'react-router-dom';
 
 import Header from '../../componentes/Header';
 import Nota from '../../componentes/Nota';
+
+import imagem from '../../resources/gas-station.png';
+
+import MarkerIcon from '../../resources/marker.png';
 
 import './styles.css';
 
 // import logo from '../../assets/logo.svg';
 
 const Estabelecimento = () => {
-  return (
+
+    const [posicao, setPosicao] = useState([-22.9108709, -43.2190417]);
+    const [nota, setNota] = useState(3);
+
+    const greenIcon = L.icon({
+        iconUrl: MarkerIcon,
+        iconSize:     [55, 55]
+    });
+
+    return (
         <div className="pagina">
-        <Header/>
-        <main>
-            <h1>Posto do Seu Zé</h1>
-            <p>O melhor posto da região!</p>
+            <Header/>
+            <main>
 
-            <Nota/>
+                <Map className="map" center={posicao} zoom={17} animate={false} zoomControl={false}>
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={posicao} icon={greenIcon}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                    </Marker>
+                </Map>
 
-            <p>Rua Melo Augusta, RJ, Rio de Janeiro</p>
-            
-            <Link to="#">
-                Avaliar estabelecimento
-            </Link>
+                <h1>Posto do Seu Zé</h1>
+                <p>O melhor posto da região!</p>
 
-            <h2>Disponibilidades:</h2>
-
-            <h2>Avaliações:</h2>
-
-            <div>
-                <h3>Marcos Antonio</h3>
                 <Nota/>
-                <p>Bem okay.</p>
-            </div>
 
-        </main>
-    </div>
-  )
+                <p>Rua Melo Augusta, RJ, Rio de Janeiro</p>
+                
+                <Link to="#">
+                    Avaliar estabelecimento
+                </Link>
+
+                <h2>Disponibilidades:</h2>
+
+                <div>
+                    <img src={imagem} alt="gas"/>
+                    <p>Gasolina</p>
+                </div>
+                               
+
+                <h2>Avaliações:</h2>
+
+                <div>
+                    <h3>Marcos Antonio</h3>
+                    <Nota/>
+                    <p>Bem okay.</p>
+                </div>
+
+            </main>
+        </div>
+    )
 }
 
 export default Estabelecimento;
